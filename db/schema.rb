6575857f36c9541c525a2a068fb341551727a9d3
Tim Halliday <thalliday793@gmail.com>
2022-02-28 14:59:31 +0000

    basic models created

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,61 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_28_144208) do
+ActiveRecord::Schema.define(version: 2022_02_28_145912) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookmarks", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "resource_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["resource_id"], name: "index_bookmarks_on_resource_id"
+    t.index ["user_id"], name: "index_bookmarks_on_user_id"
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "resource_id", null: false
+    t.text "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["resource_id"], name: "index_comments_on_resource_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
+  create_table "likes", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "resource_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["resource_id"], name: "index_likes_on_resource_id"
+    t.index ["user_id"], name: "index_likes_on_user_id"
+  end
+
+  create_table "resources", force: :cascade do |t|
+    t.string "source"
+    t.string "url"
+    t.string "author"
+    t.string "title"
+    t.integer "length"
+    t.date "published_on"
+    t.string "type"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.text "content"
+    t.integer "rating"
+    t.bigint "user_id", null: false
+    t.bigint "resource_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["resource_id"], name: "index_reviews_on_resource_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -23,8 +74,33 @@ ActiveRecord::Schema.define(version: 2022_02_28_144208) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.text "bio"
+    t.integer "age"
+    t.string "native_language"
+    t.string "target_language"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  create_table "views", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "resource_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["resource_id"], name: "index_views_on_resource_id"
+    t.index ["user_id"], name: "index_views_on_user_id"
+  end
+
+  add_foreign_key "bookmarks", "resources"
+  add_foreign_key "bookmarks", "users"
+  add_foreign_key "comments", "resources"
+  add_foreign_key "comments", "users"
+  add_foreign_key "likes", "resources"
+  add_foreign_key "likes", "users"
+  add_foreign_key "reviews", "resources"
+  add_foreign_key "reviews", "users"
+  add_foreign_key "views", "resources"
+  add_foreign_key "views", "users"
 end
