a5cb3de54ad0614d813a7ad04f4535cee469bb9f
Susannah Taylor <susannahtay@gmail.com>
2022-03-01 14:40:45 +0000

    videos controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   resources :users, only: :show
+  resources :videos, only: :show
 
   resources :resources, only: [:show, :index, :new, :create] do
     resources :likes, only: [:create, :destroy]
