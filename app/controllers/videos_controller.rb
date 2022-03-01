65d8e4cdb61c357588033dcbe7e2db930da5c7e3
Susannah Taylor <susannahtay@gmail.com>
2022-03-01 17:53:05 +0000

    working on cards

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,6 +1,5 @@
 class VideosController < ApplicationController
   def show
     @video = Video.find(params[:id])
-    @url_id = @video.url.split("?v=")[1]
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,7 @@ Rails.application.routes.draw do
     resources :reviews, only: [:create]
     resources :comments, only: [:create]
   end
+
   resources :likes, only: :destroy
   resources :bookmarks, only: :destroy
   resources :reviews, only: :destroy
