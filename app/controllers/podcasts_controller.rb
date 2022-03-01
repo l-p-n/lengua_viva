1e553e934bf2b3c423956c696747464437ae32f8
Tim Halliday <thalliday793@gmail.com>
2022-03-01 15:10:17 +0000

    embedded player on view page

--- app/controllers/podcasts_controller.rb
+++ app/controllers/podcasts_controller.rb
@@ -1,2 +1,13 @@
 class PodcastsController < ApplicationController
+  before_action :find_podcast, only: [:show]
+
+  def show
+
+  end
+
+  private
+
+  def find_podcast
+    @podcast = Podcast.find(params[:id])
+  end
 end
