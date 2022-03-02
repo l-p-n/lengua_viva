2f463c13152c610f4bc68b09cf7279ce12130bfc
Tim Halliday <thalliday793@gmail.com>
2022-03-02 14:53:16 +0000

    basic like functionality implemented

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -1,6 +1,15 @@
 class ResourcesController < ApplicationController
-
   def index
     @resources = Resource.all
+  end
+
+  def like
+    @resource = Resource.find(params[:id])
+    Like.create(user: current_user, resource: @resource)
+    redirect_to @comment.resource
+  end
+
+  def unlike
+    @resource = Resource.find(params[:id])
   end
 end
--- app/models/like.rb
+++ app/models/like.rb
@@ -2,5 +2,5 @@ class Like < ApplicationRecord
   belongs_to :user
   belongs_to :resource
 
-  validates :user, :resource, presence: true
+  validates :user, uniqueness: {scope: :resource}
 end
