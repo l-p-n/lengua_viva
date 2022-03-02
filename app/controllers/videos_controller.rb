40e6ae98a4abf871b55d66e90905bfc7afd68215
Luiza Passaglia Nascimento <luizapn@hotmail.com>
2022-03-02 15:42:50 +0000

    show pages style streamlined

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,7 +1,8 @@
 class VideosController < ApplicationController
   def show
+    @resource_show_page = true
     @comment = Comment.new
-    @resource = Video.find(params[:id])
+    @resource = Resource.find(params[:id])
     @url_id = @resource.url.split("?v=")[1]
   end
 end
