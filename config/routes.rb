72fa2389c545743e51a179d7342ba05737809e45
Tim Halliday <thalliday793@gmail.com>
2022-03-04 13:58:45 +0000

    edit view and controllers

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/users/:id/my_account', to: 'my_account#show', as: 'my_account'
 
-  resources :users, only: :show
+  resources :users, only: [:show, :edit, :update]
   resources :videos, only: :show
   resources :songs, only: :show
   resources :podcasts, only: :show
