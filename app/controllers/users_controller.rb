46ccd5be369a6fea1721eb9a1836ed127d7fe541
Tim Halliday <thalliday793@gmail.com>
2022-03-04 11:49:48 +0000

    created basic view page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
   def my_account
-
+    @user = User.find(params[:id])
+    @bookmarked = @user.bookmarks.map { |bookmark| bookmark.resource }
+    @viewed = @user.views.map { |view| view.resource }
   end
 end
