d010f88ec1b12e77db075bd2dbc5da491d5facd0
Tim Halliday <thalliday793@gmail.com>
2022-03-04 12:19:56 +0000

    updated controllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,3 @@
 class UsersController < ApplicationController
-  def my_account
-    @user = User.find(params[:id])
-    @bookmarked = @user.bookmarks.map { |bookmark| bookmark.resource }
-    @viewed = @user.views.map { |view| view.resource }
-  end
+
 end
