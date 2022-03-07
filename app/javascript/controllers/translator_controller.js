0f145d4827b052263addfe4dff11d0cb5aa1227f
Tim Halliday <thalliday793@gmail.com>
2022-03-07 17:36:20 +0000

    responsive icon

--- app/javascript/controllers/translator_controller.js
+++ app/javascript/controllers/translator_controller.js
@@ -4,7 +4,7 @@ export default class extends Controller {
   static targets = ["input", "results"]
 
   fetchTranslation(word) {
-    this.resultsTarget.innerHTML = `<a href="https://www.wordreference.com/es/en/translation.asp?spen=${this.inputTarget.value}" id="dictionary-link"><strong>${this.inputTarget.value}</strong></a>`
+    this.resultsTarget.innerHTML = `<a href="https://www.wordreference.com/es/en/translation.asp?spen=${this.inputTarget.value}" target="_blank" id="dictionary-link"><strong>${this.inputTarget.value}</strong></a>`
     fetch(`https://www.dictionaryapi.com/api/v3/references/spanish/json/${word}?key=388f7298-4a72-455e-811c-997fb974223e`)
     .then(response => response.json())
     .then(data =>
