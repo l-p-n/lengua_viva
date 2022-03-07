1d6ba01261cbbfdfd541c6873d4a0d74d3801c0d
Tim Halliday <thalliday793@gmail.com>
2022-03-07 12:41:30 +0000

    all show pages complete

--- app/controllers/podcasts_controller.rb
+++ app/controllers/podcasts_controller.rb
@@ -4,6 +4,7 @@ class PodcastsController < ApplicationController
     @comment = Comment.new
     @review = Review.new
     @resource = Resource.find(params[:id])
+    View.create(user: current_user, resource: @resource)
     @like = Like.find_by(user: current_user, resource: @resource)
     @bookmark = Bookmark.find_by(user: current_user, resource: @resource)
   end
