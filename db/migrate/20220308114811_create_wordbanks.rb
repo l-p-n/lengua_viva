e634e48072ce7f689cab7076cf9f73a85f877541
Tim Halliday <thalliday793@gmail.com>
2022-03-08 11:49:15 +0000

    created models

---
+++ db/migrate/20220308114811_create_wordbanks.rb
@@ -0,0 +1,9 @@
+class CreateWordbanks < ActiveRecord::Migration[6.1]
+  def change
+    create_table :wordbanks do |t|
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
---
+++ db/migrate/20220308114836_create_words.rb
@@ -0,0 +1,12 @@
+class CreateWords < ActiveRecord::Migration[6.1]
+  def change
+    create_table :words do |t|
+      t.string :english_def
+      t.string :spanish_def
+      t.notes :extra_info
+      t.references :wordbank, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
