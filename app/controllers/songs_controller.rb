8dfc48a6ead762e69a6556456294e2680de5cc85
Susannah Taylor <susannahtay@gmail.com>
2022-03-09 11:17:02 +0000

    related content exclude current

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -5,8 +5,12 @@ class SongsController < ApplicationController
     @review = Review.new
     @resource = Resource.find(params[:id])
     View.create(user: current_user, resource: @resource)
-    @related_resources = Resource.tagged_with(@resource.tags, wild: true, any: true)
+
+    @related_resources_all = Resource.tagged_with(@resource.tags, wild: true, any: true)
+    @related_resources = @related_resources_all.reject { |resource| resource == @resource }
+
     @like = Like.find_by(user: current_user, resource: @resource)
     @bookmark = Bookmark.find_by(user: current_user, resource: @resource)
+
   end
 end
