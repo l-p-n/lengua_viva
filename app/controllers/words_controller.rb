70f2eea00981e359da9fe04d7eafef288eac389f
Tim Halliday <thalliday793@gmail.com>
2022-03-09 11:43:06 +0000

    Basic AJAX done

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -4,7 +4,7 @@ class WordsController < ApplicationController
     @wordbank = Wordbank.find_by(user: current_user)
     @word.wordbank = @wordbank
     if @word.save
-      redirect_to my_classroom_path
+      redirect_to my_classroom_path(current_user, anchor: "my-vocabulary-header")
     else
       render my_classroom_path
     end
