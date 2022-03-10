19bf3b055d624702282cbc88edde98fd5c798718
Tim Halliday <thalliday793@gmail.com>
2022-03-10 14:35:47 +0000

    changed notes and bookmarks order

--- app/controllers/my_account_controller.rb
+++ app/controllers/my_account_controller.rb
@@ -2,6 +2,7 @@ class MyAccountController < ApplicationController
   def show
     @user = current_user
     @bookmarked = @user.bookmarks.map { |bookmark| bookmark.resource }
+    @bookmarked = @bookmarked.reverse
     @viewed = @user.views.map { |view| view.resource }
     @viewed = @viewed.reverse.uniq
 
